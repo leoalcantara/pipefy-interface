fc6818b5b7b3f2047fe6c44faf7f6a7d0ebe440e
leoalcantara <leonidio.ti@gmail.com>
2020-07-27 15:38:22 -0300

    drag function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import GlobalStyled from './styles/global';
+
 import Header from './components/Header';
 import Board from './components/Board';
 
 function App() {
   return (
-    <>      
+    <DndProvider backend={HTML5Backend} >           
       <Header />
       <Board />
       <GlobalStyled />
-    </>
+    </DndProvider>
   );
 }
 
